2e1e5537538d39b4d720b75937c86e095978ea17
matt evanoff <rtpmatt@gmail.com>
2012-03-15 22:33:35 -0400

    updates

--- autoplay.js
+++ autoplay.js
@@ -1,60 +1,174 @@
-javascript:(function(){
-	var eppsWatched = 1,
-		movieIDrgx = /movieid=\d*[&amp;|#]/,
-		trackIDrgx = /trkid=\d*[&amp;|#]/,
-		episoIDrgx = /episodeMovieId=\d*[&amp;|#]/,
-		idregx = /\d+/,
-		epps, movieId, trkId, eppId, sl, tmp;
+javascript:(function(netflix, undefined) {
+	var seasonId = 0,
+	episodeId = 0,
+	numWatched = 0,
+	numToWatch = 3,
+	epIdRegex = /,EpisodeMovieId=\d*,/,
+	idregx = /\d+/,
+	done = false,
+	sl,
+	init,
+	currrentEpisodeId,
+	currentMovieId,
+	seasons,
+	showData,
+	waitTimer,
+	node;	
 
-	//grab netflix JS interface for player
+	if(!netflix || !netflix.Silverlight || !netflix.Silverlight.MoviePlayer || !netflix.Silverlight.MoviePlayer.getPlugin() || !netflix.Silverlight.MoviePlayer.getPlugin().settings.initParams) {
+		alert('You do not appear to have a show playing, please start a show first');
+		return;
+	}
+
+	//grab the things we need
+	sl = netflix.Silverlight.MoviePlayer.getPlugin().getScriptInterface();
+	init = netflix.Silverlight.MoviePlayer.getPlugin().settings.initParams;
+	currrentEpisodeId = parseInt(idregx.exec(epIdRegex.exec(init)), 10);
+	currentMovieId = parseInt(netflix.Silverlight.MoviePlayer.getPlugin().settings.movieId, 10);
+
+	//Check if the user has already loded teh bookmarklet
+	var autoplayElement = document.getElementById('NetflixAutoplay');
+	if(autoplayElement) {
+		alert('You have already loaded the autoplay bookmarklet, click the text at the botton to change number of episodes.');
+		return;
+	}
+
+	//grab the metadata and decode it
 	try {
-		sl = netflix.Silverlight.MoviePlayer.getPlugin().getScriptInterface() || false;
-	} catch (e) {
-		sl = false;
-	}		
+		showData = JSON.parse(decode64(netflix.Silverlight.MoviePlayer.getPlugin().settings.metadata));
+	} catch(e) {
+		alert('Error processing data =(');
+		return;
+	}
 
-	if(!sl) {
-		alert('Please start the first episode you wish to watch.');
-	} else {
-		epps = prompt('How many episodes would you like to play?', '3'),
-		//number of episodes the user wants to watch
-		epps = parseInt(epps, 10);
+	if(showData.Movie) {
+		alert('This appears to be a movie not a TV show.  This bookmarklet only works on TV show.');
+		return;
+	}
 
-		//extract the IDs from the url
-		movieId = parseInt((idregx.exec(movieIDrgx.exec(window.location)) || [0])[0], 10);
-		eppId 	= parseInt((idregx.exec(episoIDrgx.exec(window.location)) || [0])[0], 10);
-		trkId 	= parseInt((idregx.exec(trackIDrgx.exec(window.location)) || [0])[0], 10);
-
-		//add even to movie finished
-		sl.OnMovieWatched = function() {
-			var waiting = null; //make our timeer
-
-			//check if we have finished watching
-			if(eppsWatched < epps) {
-				//move to the next episode
-				if(trkId !== 0) {
-					trkId++;
-				}
-				if(eppId !== 0) {
-					eppId++;
-				}
-				if(movieId !== 0) {
-					movieId++;
-				}			
-
-				if(!waiting) { //this event triggers about 2 minutes before the end of a show - and multiple times, my guess it is how they move their index to know they should start with the next epp next time
-					waiting = setTimeout(function() {
-						//tell netflix to play the next epp
-						sl.PlayMovie({movieId: movieId, episodeMovieId: eppId, trackId: trkId});
-
-						//increment our episode counter
-						eppsWatched++;
-
-						clearTimeout(waiting);
-						waiting = null;
-					},2*60*1000);
-				}
+	//set our pointest to match the episode we are currently on
+	seasons = showData.Show.Seasons;
+	for(seasonId = 0; seasonId < seasons.length; seasonId++) {
+		for(episodeId = 0; episodeId < seasons[seasonId].Episodes.length; episodeId++) {
+			if(seasons[seasonId].Episodes[episodeId].MovieId === currentMovieId || seasons[seasonId].Episodes[episodeId].MovieId === currrentEpisodeId) {
+				done = true;
+				break;
 			}
 		}
+		if(done) {
+			break;
+		}
 	}
-})();
+
+	//check if we were able to find the episode the user is  on
+	if(seasonId === seasons.length) {
+		alert('Error: Already of final episode, or episode data could not be found.');
+		return;
+	}	
+
+	//Prompt user for number of episodes
+	function getNumberOfEpisodesToWatch() {
+		var newNum;
+		do {
+			newNum = prompt('How many episodes would you like to play?', (numToWatch - numWatched));
+		} while (isNaN(newNum));
+
+		numWatched = 0;
+		numToWatch = parseInt(newNum, 10);
+
+		//set the text
+		if(numToWatch > 0) {
+			autoplayElement.innerHTML = 'Netflix autoplay on, Episodes left: ' + numToWatch;
+		} else {
+			autoplayElement.innerHTML = 'Netflix autoplay off';
+		}
+	}	
+
+	//create the text that shows how many episodes left & insert it
+	node = document.createElement('span');
+	autoplayElement = document.body.appendChild(node);
+	autoplayElement.id = 'NetflixAutoplay';
+	autoplayElement.innerHTML = 'Netflix autoplay on, Episodes left: ' + numToWatch;
+
+	//attach a click handler so people can change number of episodes
+	autoplayElement.addEventListener('click', getNumberOfEpisodesToWatch, false);
+
+	//prompt the user for number of episodes for the first time
+	getNumberOfEpisodesToWatch();
+
+	//handle when the episode ends
+	sl.OnMovieWatched = function() {
+		if(numWatched < numToWatch && !waitTimer) {			//Check if done autoplaying
+			waitTimer = setTimeout(function() {				//Set our timer so we do not end early
+				var epp, numLeft;
+
+				//move episode/season counters properly
+				if(seasons[seasonId].Episodes[episodeId+1]) {
+					episodeId++;
+				} else {
+					episodeId = 0;
+					seasonId++;
+				}	
+
+				//if there is a next episode, grab it
+				if(seasons[seasonId] && seasons[seasonId].Episodes[episodeId]) {
+					epp = seasons[seasonId].Episodes[episodeId];
+				}
+
+				//if there is a next episode, play it and update the text
+				if (epp) {
+					sl.PlayMovie({movieId: epp.MovieId, episodeMovieId: 0, trackId: 0});
+					numWatched++;
+					numLeft = numToWatch - numWatched;
+					if(numLeft > 0) {
+						autoplayElement.innerHTML = 'Netflix autoplay on, Episodes left: ' + numLeft;
+					} else {
+						autoplayElement.innerHTML = 'Netflix autoplay completed.';
+					}
+				}
+
+				//cleanup
+				clearTimeout(waitTimer);
+				waitTimer = null;
+			}, 2*60*1000);
+		}
+	};
+
+	//This is just a bse64 decoder
+	function decode64(input) {
+		var keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=',
+		output = '',
+		chr1, chr2, chr3 = '',
+		enc1, enc2, enc3, enc4 = '',
+		i = 0,
+		base64test = /[^A-Za-z0-9\+\/\=]/g;
+
+		input = input.replace(base64test, '');
+
+		do {
+			enc1 = keyStr.indexOf(input.charAt(i++));
+			enc2 = keyStr.indexOf(input.charAt(i++));
+			enc3 = keyStr.indexOf(input.charAt(i++));
+			enc4 = keyStr.indexOf(input.charAt(i++));
+
+			chr1 = (enc1 << 2) | (enc2 >> 4);
+			chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+			chr3 = ((enc3 & 3) << 6) | enc4;
+
+			output = output + String.fromCharCode(chr1);
+
+			if (enc3 != 64) {
+				output = output + String.fromCharCode(chr2);
+			}
+			if (enc4 != 64) {
+				output = output + String.fromCharCode(chr3);
+			}
+
+			chr1 = chr2 = chr3 = '';
+			enc1 = enc2 = enc3 = enc4 = '';
+
+		} while (i<input.length);
+
+		return unescape(output);
+	}
+})(window.netflix);
